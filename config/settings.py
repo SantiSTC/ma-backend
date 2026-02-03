14e8ffebfc5bde4bd52fb3a92cd4112670035f68
Angel <plutin.angelplu@gmail.com>
2026-02-03 13:44:45 -0500

    Agregar 'rest_framework_simplejwt.token_blacklist' para permitir el logout y la invalidación de tokens

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',  # Para logout (invalidar tokens)
     'corsheaders',
 ]
 
